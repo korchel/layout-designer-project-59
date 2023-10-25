0b20fbb8bb30cd44cb35fdcbe889cf2099a9b743
Elena Korchuganova <korchel21@gmail.com>
2023-10-25 21:15:44 +0300

    basic project structure

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const { src, dest, parallel, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+const browserSyncJob = () => {
+  browserSync.init({
+    server: "build/"
+  });
+
+  watch('app/sass/*.scss', buildSass);
+  watch('app/pug/*.pug', buildPug);
+};
+
+const buildSass = () => {
+  console.log('Компиляция SASS');
+
+  return src('app/sass/*.scss')
+    .pipe(sass())
+    .pipe(dest('build/styles/'))
+    .pipe(browserSync.stream());
+};
+
+const buildPug = () => {
+  console.log('Компиляция Pug');
+
+  return src('app/pug/*.pug')
+    .pipe(pug())
+    .pipe(dest('build/'))
+    .pipe(browserSync.stream());
+};
+
+const development = () => {
+  console.log('development');
+
+  buildPug();
+  buildSass();
+  browserSyncJob();
+};
+
+exports.default = development;
